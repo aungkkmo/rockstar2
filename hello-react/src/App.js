7d4fb5fd35cd28b08bfcda983f9949a434b6e3e4
Aung Ko Ko <aungkokomyintoo@gmail.com>
2019-03-16 16:05:16 +0630

    First React APP

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Item from './Item';
+import Add from './Add';
 
-class App extends Component {
-  render() {
+
+
+class App extends React.Component {
+  constructor(){
+	super();
+
+	this.state = {
+		data : ['Bob','Alice','Tom','Mary']
+	}  	
+	this.add = this.add.bind(this);
+  }
+
+  add(name){
+  	var data = this.state.data;
+  	data.push(name);
+
+  	this.setState({
+  		data: data
+  	})
+  }
+
+   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          {this.state.data.map(item => <Item name={item} />)}
+        </ul>
+        <Add add={this.add}/>
       </div>
     );
   }
