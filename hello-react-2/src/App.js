dd22d717110a83c9b200d8a1537d53de2bacf84d
Aung Ko Ko <aungkokomyintoo@gmail.com>
2019-03-17 14:48:38 +0630

    Hello react 2

---
+++ hello-react-2/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import List from './List'
+import Add from './Add'
+
+
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state ={
+			data : ['Milk','Eggg'],
+		}
+	}
+
+	add = (name) =>{
+		this.setState({
+			data : [
+				...this.state.data,
+				name
+			]
+		})
+	}
+
+	remove = (name) => {
+		this.setState({
+			data : this.state.data.filter(item => item != name)
+		})
+	}
+  render (){
+    return(
+			<div>
+					<List data={this.state.data} remove ={this.remove}/>
+					<Add add={this.add}/>
+			</div>
+    	)
+  }
+}
+
+export default App;
